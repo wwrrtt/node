2b19a2e61830e9cd916629929a90380220020b06
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 08:38:20 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 启动cf tunnel
+nohup ./argo tunnel --edge-ip-version auto run --token 6fe21701-bda8-4373-b130-a908c2de3ebd  >/dev/null 2>&1 &
+
+# 启动xray
+nohup ./web run ./config.json >/dev/null 2>&1 &
+
+# 启动Python应用程序
+python3 main.py
+
+echo "----- 系统进程...----- ."
+ps -ef
+
+echo "----- 系统信息...----- ."
+cat /proc/version
+echo "----- good luck (kid).----- ."
