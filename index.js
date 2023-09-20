b1d16593918fc530319731508381f7ba837b5727
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 10:20:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,91 +2,90 @@ const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
 const express = require('express');
+const os = require('os');
+const pidusage = require('pidusage');
 
 async function downloadFile(url, filename) {
-    const path = `/tmp/${filename}`;
-    const writer = fs.createWriteStream(path);
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
-    });
-    response.data.pipe(writer);
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-    });
+  const path =   ${os.tmpdir()}/${filename}  ;
+  const writer = fs.createWriteStream(path);
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  });
+  response.data.pipe(writer);
+  await new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
 }
 
-async function runCommand(command, processName) {
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`执行命令 "${command}" 出错: ${error}`);
-                reject(error);
-            } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-                if (processName) {
-                    // 检查是否存在相关的进程
-                    exec(`ps aux | grep ${processName} | grep -v grep`, (error, stdout, stderr) => {
-                        if (stdout.includes(processName)) {
-                            console.log(`进程 "${processName}" 已经启动`);
-                            resolve();
-                        } else {
-                            console.error(`进程 "${processName}" 未能启动`);
-                            reject();
-                        }
-                    });
-                } else {
-                    console.log(`执行命令 "${command}" 成功`);
-                    resolve();
-                }
-            }
-        });
-    });
+async function runCommand(command, processName = '') {
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log(  stdout: ${stdout}  );
+    console.log(  stderr: ${stderr}  );
+    if (processName) {
+      const processes = await pidusage(processName);
+      if (processes.length > 0) {
+        console.log(  进程 "${processName}" 已经启动  );
+      } else {
+        console.error(  进程 "${processName}" 未能启动  );
+        throw new Error(  进程 "${processName}" 未能启动  );
+      }
+    } else {
+      console.log(  执行命令 "${command}" 成功  );
+    }
+  } catch (error) {
+    console.error(  执行命令 "${command}" 出错: ${error}  );
+    throw error;
+  }
 }
 
 async function main() {
-    try {
-        // 下载 cloudflared 文件，并命名为 argo
-        await downloadFile('https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64', 'argo');
+  try {
+    // 下载 cloudflared 文件，并命名为 argo
+    await downloadFile('https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64', 'argo');
 
-        // 赋予 argo 可执行权限
-        await runCommand('chmod +x /tmp/argo', '');
+    // 赋予 argo 可执行权限
+    await runCommand(  chmod +x ${os.tmpdir()}/argo  );
 
-        // 下载 web 文件
-        await downloadFile('https://github.com/wwrrtt/node/raw/main/web', 'web');
+    // 下载 web 文件
+    await downloadFile('https://github.com/wwrrtt/node/raw/main/web', 'web');
 
-        // 赋予 web 可执行权限
-        await runCommand('chmod +x /tmp/web', '');
+    // 赋予 web 可执行权限
+    await runCommand(  chmod +x ${os.tmpdir()}/web  );
 
-        // 下载 config.json 文件
-        await downloadFile('https://github.com/wwrrtt/node/raw/main/config.json', 'config.json');
+    // 下载 config.json 文件
+    await downloadFile('https://github.com/wwrrtt/node/raw/main/config.json', 'config.json');
 
-        // 下载 start.sh 文件
-        await downloadFile('https://github.com/wwrrtt/node/raw/main/start.sh', 'start.sh');
+    // 下载 start.sh 文件
+    await downloadFile('https://github.com/wwrrtt/node/raw/main/start.sh', 'start.sh');
 
-        // 赋予 start.sh 可执行权限
-        await runCommand('chmod +x /tmp/start.sh', '');
+    // 赋予 start.sh 可执行权限
+    await runCommand(  chmod +x ${os.tmpdir()}/start.sh  );
 
-        // 运行 start.sh
-        await runCommand('/tmp/start.sh');
+    // 运行 start.sh
+    await runCommand(  ${os.tmpdir()}/start.sh  );
 
-        // 启动 Express.js 应用
-        const app = express();
-        const port = 3000; //你可以根据需要更改端口号
+    // 启动 Express.js 应用
+    const app = express();
+    const port = 3000; //你可以根据需要更改端口号
 
-        app.get('/', (req, res) => {
-          res.send('Hello World!');
-        });
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
 
-        app.listen(port, () => {
-          console.log(`App listening at http://localhost:${port}`);
-        });
-    } catch (error) {
-        console.error(`出错了: ${error}`);
-    }
+    const server = app.listen(port, () => {
+      console.log(  App listening at http://localhost:${port}   );
+    });
+
+    server.on('error', (error) => {
+      console.error(  Express.js 应用启动错误: ${error}  );
+    });
+  } catch (error) {
+    console.error(  出错了: ${error}  );
+  }
 }
 
 main();
