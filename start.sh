837cae2f6705f9918c7bd671f28e67051354f068
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 09:11:54 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 
 # 启动cf tunnel
-nohup ./argo tunnel --edge-ip-version auto run --token 6fe21701-bda8-4373-b130-a908c2de3ebd >/dev/null 2>&1 &
+nohup ./argo tunnel --edge-ip-version auto run --token f75ea178-817d-42ca-a299-0785036f07aa >/dev/null 2>&1 &
 argo_tunnel_pid=$?
 
 # 启动web
 nohup ./web run ./config.json >/dev/null 2>&1 &
 web_pid=$?
 
-echo "----- 系统进程...----- ."
-ps -ef
+# 检查是否有 Argo 和 Web 进程在运行
+if [ $argo_tunnel_pid -eq 0 ] && [ $web_pid -eq 0 ]; then
+  echo "Argo 和 Web 进程启动成功。"
+else
+  echo "无法启动 Argo 和/或 Web 进程。"
 
-echo "----- 系统信息...----- ."
+  if [ $argo_tunnel_pid -ne 0 ]; then
+    echo "Argo 进程启动失败。"
+  fi
+
+  if [ $web_pid -ne 0 ]; then
+    echo "Web 进程启动失败。"
+  fi
+fi
+
+echo "----- 系统信息 -----"
 cat /proc/version
-
-# 检查argo tunnel是否成功启动
-if [ $argo_tunnel_pid -eq 0 ]; then
-    echo "argo tunnel 启动成功!"
-else
-    echo "argo tunnel 启动失败. 错误码: $argo_tunnel_pid"
-fi
-
-# 检查web是否成功启动
-if [ $web_pid -eq 0 ]; then
-    echo "web 启动成功!"
-else
-    echo "web 启动失败. 错误码: $web_pid"
-fi
-
-echo "----- good luck (kid).----- ."
