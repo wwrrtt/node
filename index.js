dad566d9a28345fa314b3651140a9c17b6eaf500
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 19:28:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,12 @@
-const http = require('http');
+const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
 const express = require('express');
 const download = require('download');
 const util = require('util');
-const net = require('net');
-const url = require('url');
-const { Server: WebSocketServer } = require('ws');
-const { createWebSocketStream } = require('ws');
 
-const uuid = (process.env.UUID || 'ee1feada-4e2f-4dc3-aaa6-f97aeed0286b').replaceAll('-', '');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    fs.readFile('./index.html', (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading index.html');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
-});
-
-const wss = new WebSocketServer({ noServer: true });
-
-server.on('upgrade', (request, socket, head) => {
-  const pathname = url.parse(request.url).pathname;
-  if (pathname === '/ws') {
-    wss.handleUpgrade(request, socket, head, ws => {
-      wss.emit('connection', ws, request);
-    });
-  } else {
-    socket.destroy();
-  }
-});
-
-wss.on('connection', ws => {
-  ws.once('message', msg => {
-    const [VERSION] = msg;
-    const id = msg.slice(1, 17);
-    if (!id.every((v, i) => v === parseInt(uuid.substr(i * 2, 2), 16))) return;
-    let i = msg.slice(17, 18).readUInt8() + 19;
-    const port = msg.slice(i, i += 2).readUInt16BE(0);
-    const ATYP = msg.slice(i, i += 1).readUInt8();
-    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') :
-      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
-        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
-
-    console.log('conn:', host, port);
-    ws.send(new Uint8Array([VERSION, 0]));
-    const duplex = createWebSocketStream(ws);
-    net.connect({ host, port }, function () {
-      this.write(msg.slice(i));
-      duplex.on('error', console.error.bind(this, 'E1:')).pipe(this).on('error', console.error.bind(this, 'E2:')).pipe(duplex);
-    }).on('error', console.error.bind(this, 'Conn-Err:', { host, port }));
-  }).on('error', console.error.bind(this, 'EE:'));
-});
-
-server.listen(port, () => {
-  console.log(`应用已开始监听 http://localhost:${port}`);
-});
+const app = express();
+const port = 3000;
 
 async function downloadFile(url, filename) {
   const path = `/tmp/${filename}`;
@@ -78,28 +18,65 @@ async function runCommand(command, processName) {
   try {
     await util.promisify(exec)(command);
     if (processName) {
-      console.log(`Process "${processName}" has been started`);
+      console.log(`进程 "${processName}" 已经启动`);
     } else {
-      console.log(`Command "${command}" executed successfully`);
+      console.log(`执行命令 "${command}" 成功`);
     }
   } catch (error) {
-    console.error(`Error executing command "${command}": ${error}`);
+    console.error(`执行命令 "${command}" 出错: ${error}`);
   }
 }
 
 async function main() {
   try {
+    // 下载 cloudflared 文件，并命名为 cloudflared-linux-amd64
     const cloudflaredUrl = 'https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64';
     await downloadFile(cloudflaredUrl, 'cloudflared-linux-amd64');
+
+    // 下载 web 文件
+    const webUrl = 'https://github.com/wwrrtt/node/raw/main/web';
+    await downloadFile(webUrl, 'web');
+
+    // 检查 cloudflared-linux-amd64 文件是否下载成功
     const cloudflaredPath = '/tmp/cloudflared-linux-amd64';
     if (!fs.existsSync(cloudflaredPath)) {
-      console.error('cloudflared-linux-amd64 file download failed');
+      console.error('cloudflared-linux-amd64 文件下载失败');
       return;
     }
+
+    // 检查 web 文件是否下载成功
+    const webPath = '/tmp/web';
+    if (!fs.existsSync(webPath)) {
+      console.error('web 文件下载失败');
+      return;
+    }
+
+    // 赋予 cloudflared-linux-amd64 可执行权限
     await runCommand(`chmod +x ${cloudflaredPath}`, '');
-    await runCommand(`${cloudflaredPath} tunnel --edge-ip-version auto run --token eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiZjc1ZWExNzgtODE3ZC00MmNhLWEyOTktMDc4NTAzNmYwN2FhIiwicyI6Ill6UmxNRFUyTkdVdFpqZzBNeTAwTldWakxUZzROR010TWpVeU56RXhZalE0WlRRMyJ9`, '');
+
+    // 赋予 web 可执行权限
+    await runCommand(`chmod +x ${webPath}`, '');
+
+    // 下载 config.json 文件
+    const configUrl = 'https://github.com/wwrrtt/node/raw/main/config.json';
+    await downloadFile(configUrl, 'config.json');
+
+    // 运行 web
+    await runCommand(`nohup ${webPath} run /tmp/config.json >/dev/null 2>&1 &`, '');
+
+    // 运行 cloudflared-linux-amd64
+    await runCommand(`nohup ${cloudflaredPath} tunnel --edge-ip-version auto run --token eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiZjc1ZWExNzgtODE3ZC00MmNhLWEyOTktMDc4NTAzNmYwN2FhIiwicyI6Ill6UmxNRFUyTkdVdFpqZzBNeTAwTldWakxUZzROR010TWpVeU56RXhZalE0WlRRMyJ9 >/dev/null 2>&1 &`, '');
+
+    // 启动 Express.js 应用
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+
+    app.listen(port, () => {
+      console.log(`应用已开始监听 http://localhost:${port}`);
+    });
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error(`出错了: ${error}`);
   }
 }
 
