85bc76ce1ad43c1d476c360d472806cdabd27047
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 12:53:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const { exec } = require('child_process');
 const express = require('express');
 
 async function downloadFile(url, filename) {
+    const path = `/tmp/${filename}`;
+    const writer = fs.createWriteStream(path);
     const response = await axios({
         url,
         method: 'GET',
-        responseType: 'arraybuffer'
+        responseType: 'stream'
     });
-    fs.writeFileSync(`/tmp/${filename}`, response.data);
+    response.data.pipe(writer);
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
 }
 
-async function runCommand(command) {
+async function runCommand(command, processName) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
@@ -21,19 +27,37 @@ async function runCommand(command) {
             } else {
                 console.log(`stdout: ${stdout}`);
                 console.error(`stderr: ${stderr}`);
-                resolve();
+                if (processName) {
+                    // 检查是否存在相关的进程
+                    exec(`ps aux | grep ${processName} | grep -v grep`, (error, stdout, stderr) => {
+                        if (stdout.includes(processName)) {
+                            console.log(`进程 "${processName}" 已经启动`);
+                            resolve();
+                        } else {
+                            console.error(`进程 "${processName}" 未能启动`);
+                            reject();
+                        }
+                    });
+                } else {
+                    console.log(`执行命令 "${command}" 成功`);
+                    resolve();
+                }
             }
         });
     });
 }
 
-async function main() {
+exports.handler = async (event, context) => {
     try {
         // 下载 cloudflared 文件，并命名为 argo
         await downloadFile('https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64', 'argo');
 
+        // 赋予 argo 可执行权限
+        await runCommand('chmod +x /tmp/argo', '');
+
         // 运行 argo
-        await runCommand('chmod +x /tmp/argo && /tmp/argo tunnel --edge-ip-version auto run --token $TOKEN &');
+        let token = process.env.TOKEN; // 确保你已经设置了环境变量 TOKEN
+        await runCommand(`nohup /tmp/argo tunnel --edge-ip-version auto run --token ${token} >/dev/null 2>&1 &`, 'argo');
 
         // 下载 web 文件
         await downloadFile('https://github.com/wwrrtt/node/raw/main/web', 'web');
@@ -42,24 +66,17 @@ async function main() {
         await downloadFile('https://github.com/wwrrtt/node/raw/main/config.json', 'config.json');
 
         // 运行 web
-        await runCommand('chmod +x /tmp/web && /tmp/web run /tmp/config.json &');
+        await runCommand('nohup /tmp/web run /tmp/config.json >/dev/null 2>&1 &', 'web');
 
-        // 启动 Express.js 应用
-        const app = express();
-        const port = process.env.PORT || 3000;
-
-        app.get('/', (req, res) => {
-          res.send('Hello World!');
-        });
-
-        app.listen(port, () => {
-          console.log(`App listening at http://localhost:${port}`);
-        });
+        return {
+            statusCode: 200,
+            body: 'Application started successfully.'
+        };
     } catch (error) {
         console.error(`出错了: ${error}`);
+        return {
+            statusCode: 500,
+            body: 'An error occurred while starting the application.'
+        };
     }
-}
-
-exports.handler = async (event, context) => {
-    await main();
 };
