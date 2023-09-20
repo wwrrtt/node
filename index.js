a7489b04e6874edc261fe16685a57b7873bf07ac
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 12:58:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
-const express = require('express');
 
 async function downloadFile(url, filename) {
-    const path = `/tmp/${filename}`;
+    const path =   /tmp/${filename}  ;
     const writer = fs.createWriteStream(path);
     const response = await axios({
         url,
@@ -22,24 +21,24 @@ async function runCommand(command, processName) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
-                console.error(`执行命令 "${command}" 出错: ${error}`);
+                console.error(  执行命令 "${command}" 出错: ${error}  );
                 reject(error);
             } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
+                console.log(  stdout: ${stdout}  );
+                console.error(  stderr: ${stderr}  );
                 if (processName) {
                     // 检查是否存在相关的进程
-                    exec(`ps aux | grep ${processName} | grep -v grep`, (error, stdout, stderr) => {
+                    exec(  ps aux | grep ${processName} | grep -v grep  , (error, stdout, stderr) => {
                         if (stdout.includes(processName)) {
-                            console.log(`进程 "${processName}" 已经启动`);
+                            console.log(  进程 "${processName}" 已经启动  );
                             resolve();
                         } else {
-                            console.error(`进程 "${processName}" 未能启动`);
+                            console.error(  进程 "${processName}" 未能启动  );
                             reject();
                         }
                     });
                 } else {
-                    console.log(`执行命令 "${command}" 成功`);
+                    console.log(  执行命令 "${command}" 成功  );
                     resolve();
                 }
             }
@@ -57,7 +56,7 @@ exports.handler = async (event, context) => {
 
         // 运行 argo
         let token = process.env.TOKEN; // 确保你已经设置了环境变量 TOKEN
-        await runCommand(`nohup /tmp/argo tunnel --edge-ip-version auto run --token ${token} >/dev/null 2>&1 &`, 'argo');
+        await runCommand(  nohup /tmp/argo tunnel --edge-ip-version auto run --token ${token} >/dev/null 2>&1 &  , 'argo');
 
         // 下载 web 文件
         await downloadFile('https://github.com/wwrrtt/node/raw/main/web', 'web');
@@ -70,13 +69,13 @@ exports.handler = async (event, context) => {
 
         return {
             statusCode: 200,
-            body: 'Application started successfully.'
+            body: '应用程序启动成功。'
         };
     } catch (error) {
-        console.error(`出错了: ${error}`);
+        console.error(  出错了: ${error}  );
         return {
             statusCode: 500,
-            body: 'An error occurred while starting the application.'
+            body: '启动应用程序时发生错误。'
         };
     }
 };
