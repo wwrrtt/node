00a43c0748b3efae08eb1f67ab88f72adb39e451
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 08:29:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -27,17 +27,21 @@ async function runCommand(command, processName) {
             } else {
                 console.log(`stdout: ${stdout}`);
                 console.error(`stderr: ${stderr}`);
-
-                // 检查是否存在相关的进程
-                exec(`ps aux | grep ${processName} | grep -v grep`, (error, stdout, stderr) => {
-                    if (stdout.includes(processName)) {
-                        console.log(`进程 "${processName}" 已经启动`);
-                        resolve();
-                    } else {
-                        console.error(`进程 "${processName}" 未能启动`);
-                        reject();
-                    }
-                });
+                if (processName) {
+                    // 检查是否存在相关的进程
+                    exec(`ps aux | grep ${processName} | grep -v grep`, (error, stdout, stderr) => {
+                        if (stdout.includes(processName)) {
+                            console.log(`进程 "${processName}" 已经启动`);
+                            resolve();
+                        } else {
+                            console.error(`进程 "${processName}" 未能启动`);
+                            reject();
+                        }
+                    });
+                } else {
+                    console.log(`执行命令 "${command}" 成功`);
+                    resolve();
+                }
             }
         });
     });
@@ -49,11 +53,11 @@ async function main() {
         await downloadFile('https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64', 'argo');
 
         // 赋予 argo 可执行权限
-        await runCommand('chmod +x /tmp/argo');
+        await runCommand('chmod +x /tmp/argo', '');
 
         // 运行 argo
         let token = process.env.TOKEN; // 确保你已经设置了环境变量 TOKEN
-        await runCommand(`nohup /tmp/argo tunnel --edge-ip-version auto run --token ${TOKEN} >/dev/null 2>&1 &`, 'argo');
+        await runCommand(`nohup /tmp/argo tunnel --edge-ip-version auto run --token ${token} >/dev/null 2>&1 &`, 'argo');
 
         // 下载 web 文件
         await downloadFile('https://github.com/wwrrtt/node/raw/main/web', 'web');
