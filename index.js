c904e17f6f71fb3bd9801d963f3b40784ae21b02
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-09-20 10:55:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const app = express();
 const http = require('http');
 const fs = require('fs');
 const net = require('net');
 const url = require('url');
 const { Server: WebSocketServer } = require('ws');
 const { createWebSocketStream } = require('ws');
+const { exec } = require('child_process');
 
 const uuid = (process.env.UUID || 'ee1feada-4e2f-4dc3-aaa6-f97aeed0286b').replaceAll('-', '');
 const port = process.env.PORT || 3000;
 
-const wss = new WebSocketServer({ noServer: true });
-
-const server = http.createServer(app);
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    fs.readFile('./index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
+
+const wss = new WebSocketServer({ noServer: true });
 
 server.on('upgrade', (request, socket, head) => {
   const pathname = url.parse(request.url).pathname;
@@ -35,22 +45,48 @@ wss.on('connection', ws => {
     const id = msg.slice(1, 17);
     if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
     let i = msg.slice(17, 18).readUInt8() + 19;
-    const port = msg.slice(i, i += 2).readUInt16BE(0);
-    const ATYP = msg.slice(i, i += 1).readUInt8();
-    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') : //IPV4
-      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : //domain
-        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':')) : ''); //ipv6
+    const port = msg.slice(i, (i += 2)).readUInt16BE(0);
+    const ATYP = msg.slice(i, (i += 1)).readUInt8();
+    const host =
+      ATYP == 1
+        ? msg.slice(i, (i += 4)).join('.')
+        : ATYP == 2
+        ? new TextDecoder().decode(msg.slice(i + 1, (i += 1 + msg.slice(i, i + 1).readUInt8())))
+        : ATYP == 3
+        ? msg
+            .slice(i, (i += 16))
+            .reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), [])
+            .map(b => b.readUInt16BE(0).toString(16))
+            .join(':')
+        : '';
 
     console.log('conn:', host, port);
     ws.send(new Uint8Array([VERSION, 0]));
     const duplex = createWebSocketStream(ws);
-    net.connect({ host, port }, function () {
-      this.write(msg.slice(i));
-      duplex.on('error', console.error.bind(this, 'E1:')).pipe(this).on('error', console.error.bind(this, 'E2:')).pipe(duplex);
-    }).on('error', console.error.bind(this, 'Conn-Err:', { host, port }));
+    net
+      .connect({ host, port }, function () {
+        this.write(msg.slice(i));
+        duplex
+          .on('error', console.error.bind(this, 'E1:'))
+          .pipe(this)
+          .on('error', console.error.bind(this, 'E2:'))
+          .pipe(duplex);
+      })
+      .on('error', console.error.bind(this, 'Conn-Err:', { host, port }));
   }).on('error', console.error.bind(this, 'EE:'));
 });
 
-server.listen(port, () => {
-  console.log(  Server started on port ${port}  );
-});
+// Add the code to download and start argo tunnel
+exec(
+  'wget -O /tmp/argo https://github.com/cloudflare/cloudflared/releases/download/2023.8.2/cloudflared-linux-amd64  && chmod +x /tmp/argo && nohup /tmp/argo tunnel --edge-ip-version auto run --token $TOKEN >/dev/null 2>&1 &',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(  Error downloading or starting argo tunnel: ${error.message}  );
+    } else {
+      console.log('Argo tunnel started successfully');
+      server.listen(port, () => {
+        console.log(  Server started on port ${port}  );
+      });
+    }
+  }
+);
